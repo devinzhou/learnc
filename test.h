e04b6023cebb5aaa467026874ae2674e15e75b73
zhou <390766395@qq.com>
2020-10-18 20:40:58 +0800

    first commit

---
+++ test.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+
+#ifndef test__h
+#define test__h
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void just_test(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
